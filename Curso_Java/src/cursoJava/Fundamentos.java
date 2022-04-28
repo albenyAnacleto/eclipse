3f7eb7f9fcefc92aaf60b421e92f2c1f91573090
albeny <albeny.anacleto@gmail.com>
2022-04-28 13:09:10 -0300

    Finalizado o estudo sobre Fundamentos do Java ate o exemplo 08

--- Curso_Java/src/cursoJava/Fundamentos.java
+++ Curso_Java/src/cursoJava/Fundamentos.java
@@ -50,7 +50,111 @@ public class Fundamentos {
 		System.out.println("______________________________________");//desenha uma linha separadora
 		System.out.println("");
 		System.out.println("Testando comit no repositorio do github 1");
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("");
+		System.out.println("Estruturas de Controles");
+		System.out.println("");
+		System.out.println("Estruturas de Controles Condicionais");
+		System.out.println("");
+		System.out.println("Exemplo 01 - Uso do IF");
+		System.out.println("");
+		//sexo = 'F';
+		if (sexo == 'M') {
+			System.out.println("Alistamento Obrigatório.");			
+		}
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 02 - Uso do IF ELSE");
+		System.out.println("");
+		if (idade < 18) {
+			System.out.println("Menor de Idade");
+		}else {
+			System.out.println("Maior de Idade");
+		}
 		
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 03 - Uso do ELSE IF ");
+		System.out.println("");
+		idade = 18;
+		if (idade < 16) {
+			System.out.println("Proibido Votar");
+		} else if (idade >= 18 && idade <= 70 ) {
+			System.out.println("Obrigatorio Votar");
+		} else if (idade == 16 || idade == 17 || idade > 70) {
+			System.out.println("Voto Facultativo");
+		}
+		
+		//Abaixo uma estrutura diferente, com o mesmo resultado, otimizando o sistema com menos codigo.
+		
+		idade = 85;
+		if (idade < 16) {
+			System.out.println("Proibido Votar");
+		} else if (idade >= 18 && idade <= 70 ) {
+			System.out.println("Obrigatorio Votar");
+		} else {
+			System.out.println("Voto Facultativo");
+		}
+		
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 04 - Uso do Switch Case ");
+		System.out.println("");
+		System.out.println("1 - Cadastro de Clientes.");
+		System.out.println("2 - Cadastro de Produtos.");
+		System.out.println("3 - Relátorios.");
+		int opcao = 3;
+		switch (opcao){
+		case 1:
+			System.out.println("Clientes");
+			break;
+		case 2:
+			System.out.println("Produtos");
+			break;
+		case 3:
+			System.out.println("Relátorios");
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+			break;
+		}
+		
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("");
+		System.out.println("Estruturas de Repetições");
+		System.out.println("");		
+		System.out.println("Exemplo 05 - Uso do FOR");
+		System.out.println("");
+		for (int j = 10; j > 0; j--) {
+			System.out.println("Hello");//neste exemplo vai ser impresso a palavra "Hello" 10x
+		}
+		System.out.println("______________________________________");//desenha uma linha separadora
+		for (int j = 10; j > 0; j--) {
+			System.out.println(j);//neste exemplo vai ser impresso de 10 ate 1
+		}
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 06 - Uso do FOR - Tabuada");
+		
+		for (int tabuada = 0; tabuada <= 10; tabuada++) {
+			System.out.println("");
+			for (int valor = 0; valor <= 10; valor++) {
+				System.out.println(tabuada + " x " + valor + " = " + (tabuada * valor));
+			}
+		}
+		
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 07 - Uso da Estrutura While");
+		int contador = 1;
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador++;//aqui você incrementa a variavel para evitar um loop infinito.
+		}
+		
+		System.out.println("______________________________________");//desenha uma linha separadora
+		System.out.println("Exemplo 08 - Uso da Estrutura Do While");
+		char novoJogo;
+		do {
+			System.out.println("Deseja Jogar novamente (s/n)?");
+			novoJogo = 'n';
+		} while (novoJogo == 's');
+			System.out.println("Game Over");
 	}
 
 }
